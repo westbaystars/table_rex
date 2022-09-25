de8906ebb57bc3e99d8b33a7482a329874216b23
Michael Westbay <westbaystars@gmail.com>
2022-09-25 10:53:45 +0900

    Quick and dirty patch to force full-width check on on widths.

--- lib/table_rex/column.ex
+++ lib/table_rex/column.ex
@@ -3,9 +3,22 @@ defmodule TableRex.Column do
   Defines a struct that represents a column's metadata
 
   The align field can be one of :left, :center or :right.
-  """
 
-  defstruct align: :left, padding: 1, color: nil, width_calc: &String.length/1
+  Default string width calcuation to use Unicode.
+  """
+  alias Unicode.EastAsianWidth, as: Width
+
+  defstruct align: :left, padding: 1, color: nil, width_calc: &TableRex.Column.string_width/1
 
   @type t :: %__MODULE__{}
+
+  def string_width(string) do
+    width = string
+    |> String.to_charlist()
+    |> Enum.map(&Width.east_asian_width_category(&1))
+    |> Enum.reduce(0, fn width, acc -> if(width == :w, do: acc + 2, else: acc + 1) end)
+
+    #IO.inspect("#{string} -> #{width}")
+    width
+  end
 end
